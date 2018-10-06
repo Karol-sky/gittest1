9815cc978f8587e6696c3acc737632216d77452d
Karol Latarski <k.latarski@gmail.com>
2018-10-06 14:58:53 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("hello git world");
+    }
+}
